1f2306b2b2908f97979c12cde27e96685a9ea5ee
Roman Pashkovsky <petrodzher@gmail.com>
2022-08-30 17:34:13 +0300

    minor fix in categories layout

--- src/routes/home/lists/create/+page.svelte
+++ src/routes/home/lists/create/+page.svelte
@@ -328,7 +328,7 @@
 	</div>
 	{#if isAnyItemsChecked}
 		<div
-			class="absolute bottom-0 left-0 right-0 bg-white p-4 flex justify-end items-start"
+			class="fixed bottom-0 left-0 right-0 bg-white p-4 flex justify-end items-start z-20"
 			style="box-shadow: 0px -2px 12px -1px rgba(0,0,0,0.41);"
 		>
 			<div class="mr-3">Change category to:</div>

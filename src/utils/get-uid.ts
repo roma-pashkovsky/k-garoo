3725e8c19494c76d0b44d90ac7612a497dc6e875
Roman Pashkovsky <petrodzher@gmail.com>
2022-10-14 19:51:27 +0300

    fix in get uuid method

--- src/utils/get-uid.ts
+++ src/utils/get-uid.ts
@@ -1,5 +1,5 @@
 export const getUID = (): string => {
-	if (crypto) {
+	if (crypto && crypto.randomUUID) {
 		return crypto.randomUUID();
 	} else {
 		return '' + new Date().getTime() + Math.floor(Math.random() * 10000);

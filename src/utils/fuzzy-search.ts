d45f8a3c0b24254c9444388b0c0c2372c5b17555
Roman Pashkovsky <petrodzher@gmail.com>
2022-11-05 17:31:25 +0200

    removed unused code

--- src/utils/fuzzy-search.ts
+++ src/utils/fuzzy-search.ts
@@ -12,11 +12,6 @@ export class FuzzySearch<T> {
 	private _fuze: any;
 	constructor(private items: T[], options: FuzzyOptions<T>) {
 		this._fuze = new Fuse(items, options as any as Fuse.IFuseOptions<any>);
-		if (!this._fuze) {
-			setTimeout(() => {
-				this._fuze = new (window as any).Fuse(items, options);
-			}, 4000);
-		}
 	}
 
 	public search(t: string): { item: T; score?: number }[] {

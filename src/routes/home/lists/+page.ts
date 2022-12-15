a28b75346964ca5c8ec3d1422dfd8cb541447345
Roman Pashkovsky <petrodzher@gmail.com>
2022-12-15 11:22:09 +0200

    optimize page fetches

--- src/routes/home/lists/+page.ts
+++ src/routes/home/lists/+page.ts
@@ -1,12 +1,12 @@
 import { loadListItems } from '../../../stores/checklist-main-list/checklist-main-list-store';
 import { browser } from '$app/environment';
 import type { Load } from '@sveltejs/kit';
-import { auth, loadUserIfNotResolved } from '../../../stores/login/auth';
+import { auth } from '../../../stores/login/auth';
 import { loadSharedListIds } from '../../../stores/my-shared-lists/my-shared-list.store';
 import { get } from 'svelte/store';
 
-export const load: Load = async ({ fetch }) => {
-	await loadUserIfNotResolved(fetch, browser);
+export const load: Load = async ({ parent, fetch }) => {
+	await parent();
 	await loadListItems(browser, fetch);
 	if (get(auth).user) {
 		loadSharedListIds(fetch);

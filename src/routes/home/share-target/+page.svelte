d20b5982f5e193138349de3c7cb6b3bdaf8112f1
Roman Pashkovsky <petrodzher@gmail.com>
2023-01-26 15:14:43 +0200

    added share target url

---
+++ src/routes/home/share-target/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	let url: string;
+	let title: string;
+	let text: string;
+
+	onMount(() => {
+		url = $page.url.searchParams.get('url');
+		title = $page.url.searchParams.get('title');
+		text = $page.url.searchParams.get('text');
+	});
+</script>
+
+<div>
+	<div>
+		<span>Url: </span> <span>{url}</span>
+		<span>Title: </span> <span>{title}</span>
+		<span>Text: </span> <span>{text}</span>
+	</div>
+</div>
